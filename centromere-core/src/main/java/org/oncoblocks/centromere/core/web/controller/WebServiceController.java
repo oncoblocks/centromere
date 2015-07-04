f97a67032e6657bf13ad384bd60c31451b82c02d
woemler <woemler@blueprintmedicines.com>
2015-07-04 15:21:14 -0400

    Added generic service and tests.

---
+++ centromere-core/src/main/java/org/oncoblocks/centromere/core/web/controller/WebServiceController.java
@@ -0,0 +1,10 @@
+package org.oncoblocks.centromere.core.web.controller;
+
+import java.io.Serializable;
+
+/**
+ * @author woemler
+ */
+public interface WebServiceController<T, ID extends Serializable> {
+	
+}
---
+++ centromere-core/src/test/java/org/oncoblocks/centromere/core/test/web/service/GenericServiceTests.java
@@ -0,0 +1,367 @@
+package org.oncoblocks.centromere.core.test.web.service;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.oncoblocks.centromere.core.repository.GenericMongoRepository;
+import org.oncoblocks.centromere.core.repository.QueryCriteria;
+import org.oncoblocks.centromere.core.test.config.TestMongoConfig;
+import org.oncoblocks.centromere.core.test.models.Gene;
+import org.oncoblocks.centromere.core.web.service.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author woemler
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestMongoConfig.class,
+		GenericServiceTests.GenericServiceTestConfig.class})
+@FixMethodOrder
+public class GenericServiceTests {
+
+	@Autowired private GeneRepository geneRepository;
+	@Autowired private GeneService geneService;
+
+	private static boolean isConfigured = false;
+	
+	@Repository
+	public static class GeneRepository extends GenericMongoRepository<Gene, Long> {
+		@Autowired
+		public GeneRepository(MongoTemplate mongoTemplate) {
+			super(mongoTemplate, Gene.class);
+		}
+	}
+	
+	@Service
+	public static class GeneService extends GenericService<Gene, Long> {
+		@Autowired
+		public GeneService(GeneRepository repository) {
+			super(repository);
+		}
+	}
+
+	@Configuration
+	@ComponentScan(basePackages =  { "org.oncoblocks.centromere.core.test.web.service"})
+	public static class GenericServiceTestConfig { }
+
+	@Before
+	public void setup(){
+
+		if (isConfigured) return;
+
+		geneRepository.deleteAll();
+		Gene geneA = new Gene(1L, "GeneA", 9606, null, "1", null, "Test Gene A", "protein-coding", null, null, null);
+		geneA.setAttribute("isKinase:Y");
+		geneA.setAlias("ABC");
+		Gene geneB = new Gene(2L, "GeneB", 9606, null, "3", null, "Test Gene B", "protein-coding", null, null, null);
+		geneB.setAttribute("isKinase:N");
+		geneB.setAlias("DEF");
+		Gene geneC = new Gene(3L, "GeneC", 9606, null, "11", null, "Test Gene C", "pseudo", null, null, null);
+		geneC.setAttribute("isKinase:N");
+		geneC.setAlias("GHI");
+		Gene geneD = new Gene(4L, "GeneD", 9606, null, "9", null, "Test Gene D", "protein-coding", null, null, null);
+		geneD.setAttribute("isKinase:Y");
+		geneD.setAlias("JKL");
+		Gene geneE = new Gene(5L, "GeneE", 9606, null, "X", null, "Test Gene E", "pseudo", null, null, null);
+		geneE.setAttribute("isKinase:N");
+		geneE.setAlias("MNO");
+		geneRepository.insert(Arrays.asList(new Gene[] {geneA, geneB, geneC, geneD, geneE}));
+
+		isConfigured = true;
+
+	}
+
+
+	@Test
+	public void findByIdTest(){
+
+		Gene gene = geneService.findById(1L);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(1L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneA"));
+		Assert.notNull(gene.getAliases());
+		Assert.notEmpty(gene.getAliases());
+		Assert.isTrue(gene.getAliases().size() == 1);
+
+	}
+
+	@Test
+	public void findAllTest(){
+
+		List<Gene> genes = geneService.findAll();
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 5);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(1L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneA"));
+		Assert.notNull(gene.getAliases());
+		Assert.notEmpty(gene.getAliases());
+		Assert.isTrue(gene.getAliases().size() == 1);
+		System.out.println(gene.toString());
+
+	}
+
+	@Test
+	public void countTest(){
+
+		long count = geneService.count();
+		Assert.notNull(count);
+		Assert.isTrue(count == 5L);
+
+	}
+
+	@Test
+	public void findBySimpleCriteriaTest(){
+
+		List<QueryCriteria> searchCriterias = new ArrayList<>();
+		searchCriterias.add(new QueryCriteria("primaryGeneSymbol", "GeneB"));
+		List<Gene> genes = geneService.find(searchCriterias);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 1);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(2L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneB"));
+
+	}
+
+	@Test
+	public void findByNestedArrayCriteriaTest(){
+
+		List<QueryCriteria> searchCriterias = new ArrayList<>();
+		searchCriterias.add(new QueryCriteria("aliases", "DEF"));
+		List<Gene> genes = geneService.find(searchCriterias);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 1);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(2L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneB"));
+		Assert.isTrue(gene.getAliases().contains("DEF"));
+
+	}
+
+	@Test
+	public void findByNestedObjectCriteriaTest(){
+
+		List<QueryCriteria> searchCriterias = new ArrayList<>();
+		searchCriterias.add(new QueryCriteria("attributes.name", "isKinase"));
+		searchCriterias.add(new QueryCriteria("attributes.value", "Y"));
+		List<Gene> genes = geneService.find(searchCriterias);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 2);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(1L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneA"));
+		Assert.isTrue(gene.getAttributes().size() == 1);
+		Assert.isTrue(gene.getAttributes().get(0).getName().equals("isKinase"));
+		Assert.isTrue(gene.getAttributes().get(0).getValue().equals("Y"));
+
+	}
+
+	@Test
+	public void findSortedTest(){
+
+		Sort sort = new Sort(new Sort.Order(Sort.Direction.DESC, "primaryGeneSymbol"));
+		List<Gene> genes = geneService.findAllSorted(sort);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 5);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(5L));
+		Assert.isTrue(gene.getPrimaryGeneSymbol().equals("GeneE"));
+
+	}
+
+	@Test
+	public void findPagedTest(){
+
+		PageRequest pageRequest = new PageRequest(1, 2);
+		Page<Gene> page = geneService.findAllPaged(pageRequest);
+		Assert.notNull(page);
+		Assert.isTrue(page.getTotalPages() == 3);
+		Assert.isTrue(page.getTotalElements() == 5);
+
+		List<Gene> genes = page.getContent();
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 2);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(3l));
+
+	}
+
+	@Test
+	public void findByCriteriaPagedTest(){
+
+		List<QueryCriteria> searchCriterias = new ArrayList<>();
+		searchCriterias.add(new QueryCriteria("geneType", "protein-coding"));
+		PageRequest pageRequest = new PageRequest(1, 2);
+		Page<Gene> page = geneService.findPaged(searchCriterias, pageRequest);
+		Assert.notNull(page);
+		Assert.isTrue(page.getTotalElements() == 3);
+		Assert.isTrue(page.getTotalPages() == 2);
+
+		List<Gene> genes = page.getContent();
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 1);
+
+		Gene gene = genes.get(0);
+		Assert.notNull(gene);
+		Assert.isTrue(gene.getEntrezGeneId().equals(4L));
+
+	}
+
+	@Test
+	public void insertTest(){
+
+		Gene gene = new Gene(100L, "TEST", 9606, null, "1", "1", "Test gene", "protein-coding", null, null, null);
+		geneService.insert(gene);
+
+		Gene created = geneService.findById(100L);
+		Assert.notNull(created);
+		Assert.isTrue(created.getId().equals(100L));
+		Assert.isTrue(created.getPrimaryGeneSymbol().equals("TEST"));
+
+		geneService.delete(100L);
+
+	}
+
+	@Test
+	public void updateTest(){
+
+		Gene gene = new Gene(100L, "TEST", 9606, null, "1", "1", "Test gene", "protein-coding", null, null, null);
+		geneService.insert(gene);
+
+		gene.setPrimaryGeneSymbol("TEST_TEST");
+		gene.setGeneType("pseudogene");
+		geneService.update(gene);
+
+		Gene updated = geneService.findById(100L);
+		Assert.notNull(updated);
+		Assert.isTrue(updated.getPrimaryGeneSymbol().equals("TEST_TEST"));
+		Assert.isTrue(updated.getGeneType().equals("pseudogene"));
+
+		geneService.delete(100L);
+
+	}
+
+	@Test
+	public void deleteTest(){
+
+		Gene gene = new Gene(100L, "TEST", 9606, null, "1", "1", "Test gene", "protein-coding", null, null, null);
+		geneService.insert(gene);
+
+		Gene created = geneService.findById(100L);
+		Assert.notNull(created);
+		Assert.isTrue(created.getId().equals(100L));
+
+		geneService.delete(100L);
+		Gene deleted = geneService.findById(100L);
+		Assert.isNull(deleted);
+
+	}
+
+	@Test
+	public void rawJsonTestById() throws Exception {
+
+		Gene gene = new Gene();
+		gene.setEntrezGeneId(1L);
+		List<Gene> genes = geneService.find(gene);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.get(0).getEntrezGeneId().equals(1L));
+
+
+	}
+
+	@Test
+	public void rawJsonTestByGeneType() throws Exception {
+
+		Gene gene = new Gene();
+		gene.setGeneType("protein-coding");
+		List<Gene> genes = geneService.find(gene);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 3);
+		Assert.isTrue(genes.get(2).getEntrezGeneId().equals(4L));
+
+
+	}
+
+	@Test
+	public void rawJsonTestByConflictingAttributes() throws Exception {
+
+		Gene gene = new Gene();
+		gene.setGeneType("protein-coding");
+		gene.setEntrezGeneId(11L);
+
+		List<Gene> genes = geneService.find(gene);
+		Assert.notNull(genes);
+		Assert.isTrue(genes.size() == 0);
+
+
+	}
+
+	@Test
+	public void rawJsonTestByNestedAttributes() throws Exception {
+
+		Gene gene = new Gene();
+		gene.setAttribute("isKinase:Y");
+
+		List<Gene> genes = geneService.find(gene);
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 2);
+		Assert.isTrue(genes.get(0).getEntrezGeneId().equals(1L));
+
+	}
+
+	@Test
+	public void rawJsonTestByPartialNestedAttributes() throws Exception {
+
+		Gene gene = new Gene();
+		gene.setAttributeName("isKinase");
+
+		List<Gene> genes = geneService.find(gene);
+
+		Assert.notNull(genes);
+		Assert.notEmpty(genes);
+		Assert.isTrue(genes.size() == 5);
+		Assert.isTrue(genes.get(0).getEntrezGeneId().equals(1L));
+
+	}
+	
+}
